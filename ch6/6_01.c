4c352f89cb937175b740d2dddcf82eb5dcc9eb73
Consistent_Milk <mahmudulhasanfh91@gmail.com>
2022-12-10 18:11:06 +0600

    second

---
+++ ch6/6_01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// todo
+// 1. declare an int array of size 5
+// 2. init array with all 0
+// 3. pretty print index and value
+
+int main(void)
+{
+    // declaring an int array of size 5(indices 0-4)
+    int array[5];
+
+    // init with zero values
+    for (size_t i = 0; i < 5; ++i)
+    {
+        array[i] = 0;
+    }
+
+    // pretty print
+    printf("%s%8s", "Index", "Value");
+    puts("");
+    for (size_t i = 0; i < 5; ++i)
+    {
+        printf("%5d%8d\n", i, array[i]);
+    }
+}
