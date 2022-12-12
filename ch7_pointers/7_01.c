9e53dd41adc5e0d5ba7b9395c37fbb4645db7b96
Consistent_Milk <mahmudulhasanfh91@gmail.com>
2022-12-12 10:29:11 +0600

    Pointers

---
+++ ch7_pointers/7_01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 7;
+    int *aPtr = &a; // Set *aPtr to point to the address of a
+
+    printf("Address of a is %p\nValue of aPtr is %p\n\n", &a, aPtr);
+    printf("Value of a is %d\nValue of *aPtr is %d\n\n", a, *aPtr);
+    printf("Showing that * and & are complements of each other\n");
+    printf("&*aPtr = %p\n*&aPtr = %p\n", &*aPtr, *&aPtr);
+}
