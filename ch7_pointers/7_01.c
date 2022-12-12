2e90d9559b425b12609cacc4a1b6f2a34f8ba657
Consistent_Milk <mahmudulhasanfh91@gmail.com>
2022-12-12 18:09:00 +0600

    stuff

--- ch7_pointers/7_01.c
+++ ch7_pointers/7_01.c
@@ -2,11 +2,16 @@
 
 int main(void)
 {
+    // & -> The addres operator (ampersand)
+    // * -> Indirection Operator or Dereferencing Operator (asterisk)
+    puts("");
     int a = 7;
     int *aPtr = &a; // Set *aPtr to point to the address of a
 
-    printf("Address of a is %p\nValue of aPtr is %p\n\n", &a, aPtr);
-    printf("Value of a is %d\nValue of *aPtr is %d\n\n", a, *aPtr);
+    printf("Address of a is %p\n", &a);
+    printf("Value of a is %d\n", a);
+    printf("Address of aPtr is %p\n", aPtr);
+    printf("Values of *aPtr is %d\n\n", *aPtr);
     printf("Showing that * and & are complements of each other\n");
     printf("&*aPtr = %p\n*&aPtr = %p\n", &*aPtr, *&aPtr);
 }
