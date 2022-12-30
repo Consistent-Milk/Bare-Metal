1428e3e1b90e6dcf9dd8b7083ad61b13830e84fa
Consistent_Milk <mahmudulhasanfh91@gmail.com>
2022-12-30 14:27:05 +0600

    Exercises

---
+++ Fortran/Chapman - Chapter 2/Exercises/2_9/program.f90
@@ -0,0 +1,22 @@
+PROGRAM triangle
+    IMPLICIT NONE
+
+    REAL, PARAMETER :: PI = 3.1416
+    REAL :: a, b, c, theta, radian_conversion
+
+    WRITE (*,*) 'Enter the length of the hypotenuse C:'
+    READ (*,*) c
+    WRITE (*,*) 'Enter the angle THETA in degrees:'
+    READ (*,*) theta
+
+    ! COS and SIN intrinsic functions expects inputs in radians
+
+    radian_conversion = PI / 180.0
+
+    a = c * COS ( theta * radian_conversion )
+    b = c * SIN ( theta * radian_conversion )
+
+    WRITE (*,*) 'The length of the adjacent side is ', a
+    WRITE (*,*) 'The length of the opposite side is ', b
+
+END PROGRAM triangle
